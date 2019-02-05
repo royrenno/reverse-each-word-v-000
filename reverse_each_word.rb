1581ee1443557ce146c8929a0854b39ef9a35af3
roy renno <royrenno@yahoo.com>
2019-02-05 04:33:48 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
-def reverse_each_word(sentence1)
-  words=sentence.split
-  words.collect do|word|
-    word.reverse!
-  end
-  words.join("")
-end
+def reverse_each_word(sentence)	
+	words=sentence.split
+	words.collect do |word|
+	word.reverse!
+	end
+	words.join(" ")
+	end
